402119396a2fb56a9c17919b40ad80eab3e3bf33
Zhuoyang CHEN <60202079+georgedashen@users.noreply.github.com>
2023-10-25 21:58:44 +0800

    Add files via upload

---
+++ preprocessing/GAE_train_loader.py
@@ -0,0 +1,97 @@
+import argparse
+import torch
+import numpy as np
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GAE
+from torch_geometric.data import Data
+from torch_geometric.loader import NeighborLoader
+import scipy.io as sio
+import random
+import sys
+import pickle
+
+parser = argparse.ArgumentParser('GAE embedding generator for ppi')
+parser.add_argument('--org', default='human', help='organism')
+parser.add_argument('--dataset_dir', default='../Dataset', help='dir of dataset')
+parser.add_argument('--evidence', default='combined', choices = ['neighborhood', 'fusion', 'cooccurence', 'coexpression', 'experimental', 'database', 'textmining', 'combined'], help='what evidence is used to construct the PPI graph')
+args = parser.parse_args()
+
+adj = sio.loadmat(f'{args.dataset_dir}/{args.org}/{args.org}_net_{args.evidence}.mat', squeeze_me=True)
+adj = adj['Net'].todense()
+edge_index = torch.tensor(adj).nonzero().t().contiguous()
+
+feature_file = f'{args.dataset_dir}/{args.org}/features.npy'
+with open(feature_file, 'rb') as f:
+    x = pickle.load(f)
+x = torch.tensor(x).float()
+
+def set_rand_seed(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+
+set_rand_seed(42)
+
+print(f'PPI density: {len(edge_index[0])/adj.shape[0]**2}')
+print(f'feature shape: {x.shape}')
+
+class GCNEncoder(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GCNEncoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=False) # cached only for transductive learning
+        self.conv2 = GCNConv(2 * out_channels, out_channels, cached=False) # cached only for transductive learning
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        return self.conv2(x, edge_index)
+
+model = GAE(GCNEncoder(x.shape[1], 512))
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+model = model.to(device)
+
+data = Data(x=x, edge_index=edge_index)
+
+train_loader = NeighborLoader(
+            data,
+            # Sample 30 neighbors for each node for 2 iterations
+            num_neighbors=[30,15],
+            # Use a batch size of 128 for sampling training nodes
+            batch_size=128,
+            shuffle=True
+        )
+test_loader = NeighborLoader(data, num_neighbors=[-1], batch_size=128, shuffle=False)
+
+#sampled_data = next(iter(loader))
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+def train():
+    model.train()
+    total_loss = 0
+    for i, batch in enumerate(train_loader):
+        optimizer.zero_grad()
+        batch.x = batch.x.to(device)
+        batch.edge_index = batch.edge_index.to(device)
+        z = model.encode(batch.x, batch.edge_index)
+        loss = model.recon_loss(z, batch.edge_index)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(train_loader)
+
+for epoch in range(1,20):
+    loss = train()
+    print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}')
+
+model.eval()
+output = torch.zeros((adj.shape[0],512)).to(device)
+with torch.no_grad():
+    for i, batch in enumerate(test_loader):
+        batch.x = batch.x.to(device)
+        batch.edge_index = batch.edge_index.to(device)
+        z = model.encode(batch.x, batch.edge_index)
+        output[batch.input_id] = z[:batch.batch_size,:]
+
+output = output.detach().cpu().numpy() #should be 19385*512
+np.save(f'{args.dataset_dir}/{args.org}/{args.org}_net_{args.evidence}_GAE.npy', output)
---
+++ preprocessing/node2vec_train.py
@@ -0,0 +1,56 @@
+import argparse
+import torch
+import numpy as np
+from torch_geometric.nn import Node2Vec
+import scipy.io as sio
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser('node2vec embedding generator for ppi')
+parser.add_argument('--org', default='human', help='organism')
+parser.add_argument('--dataset_dir', default='../Dataset', help='dir of dataset')
+parser.add_argument('--evidence', default='combined', choices = ['neighborhood','fusion','cooccurence', 'coexpression', 'experimental', 'database', 'textmining', 'combined'], help='what evidence is used to construct the PPI graph')
+parser.add_argument('--epoch', type=int, default=20, help='organism')
+args = parser.parse_args()
+
+adj = sio.loadmat(f'{args.dataset_dir}/{args.org}/{args.org}_net_'+args.evidence+'.mat', squeeze_me=True)
+adj = adj['Net'].todense()
+edge_index = torch.tensor(adj).nonzero().t().contiguous()
+
+def set_rand_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+
+set_rand_seed(42)
+
+print(f'PPI density: {len(edge_index[0])/adj.shape[0]**2}')
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+model = Node2Vec(edge_index, embedding_dim=512, walk_length=20,
+             context_size=10, walks_per_node=10, num_nodes=adj.shape[0],
+             num_negative_samples=1, p=1, q=1, sparse=True).to(device)
+
+loader = model.loader(batch_size=128, shuffle=True, num_workers=4)
+optimizer = torch.optim.SparseAdam(list(model.parameters()), lr=0.01)
+
+def train():
+    model.train()  # put model in train model
+    total_loss = 0
+    for pos_rw, neg_rw in tqdm(loader):
+        optimizer.zero_grad()  # set the gradients to 0
+        loss = model.loss(pos_rw.to(device), neg_rw.to(device))  # compute the loss for the batch
+        loss.backward()
+        optimizer.step()  # optimize the parameters
+        total_loss += loss.item()
+    return total_loss / len(loader)
+
+
+for epoch in range(1, 20):
+    loss = train()
+    print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}')
+
+model.eval()
+output = model().cpu().detach().numpy() #should be 19385*512
+np.save(f'{args.dataset_dir}/{args.org}/{args.org}_net_'+args.evidence+'_node2vec.npy', output)
